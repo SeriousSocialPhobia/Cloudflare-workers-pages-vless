89ca20192cea32aad07c8d6e287e5126619bece5
SeriousSocialPhobia <40016950+SeriousSocialPhobia@users.noreply.github.com>
2024-05-09 18:04:23 +0800

    Update _worker.js
    change uuid

--- Vless_workers_pages/_worker.js
+++ Vless_workers_pages/_worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'c159429c-360f-4cc8-bc32-3bc027b00cbd';
+let userID = '05054471-738a-4340-8086-c6cd5005fdc7';
 
 const proxyIPs = ["cdn.xn--b6gac.eu.org"]; //workers.cloudflare.cyou bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org
 const cn_hostnames = [''];
